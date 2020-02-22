be87a23ac03ffd9bc5131b55aedc402c2ff2c1a9
justin <justinxu0223@gmail.com>
2020-02-22 23:05:35 +0800

    developing points: 1.basic server

---
+++ src/components/servers/basicServer.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+class BasicServer extends React.PureComponent {
+  state = {};
+
+  componentWillUnmount() {
+    this.apiList = [];
+    this.pollingApiList.forEach(item => {
+      clearInterval(item);
+    });
+  }
+
+  getBindList = ({ action = '', type = '' } = {}) => {
+    return this.apiList.filter(
+      item => (item.action === action && action) || (item.type === type && type),
+    );
+  };
+
+  apiList = []; // api列表
+  pollingApiList = []; // 轮训api列表
+
+  // 绑定基础方法
+  bindAction = ({
+    api = {}, // api请求
+    action = api.name, // 给doAction使用
+    type = '', // 提供getAll 遍历一起使用
+    formatBindingData = () => null, // 格式化绑定在state的值
+    bindingData = '', // 绑定在state的值
+    sendingData = {}, // 发送的数据
+    polling = false, // 当前接口是否轮询
+    callback = () => null, // 回调
+  } = {}) => {
+    this.apiList.push({
+      api,
+      action,
+      type,
+      formatBindingData,
+      bindingData,
+      sendingData,
+      polling,
+      callback,
+    });
+  };
+
+  // 执行单一请求
+  doAction = ({ action = '', type = '' } = {}) => {
+    const bindList = this.getBindList({ action, type });
+    if (!(Array.isArray(bindList) && bindList.length)) {
+      console.error('你还未绑定action');
+      return Promise.resolve(true);
+    }
+    return this.handleCommand(bindList);
+  };
+
+  handleCommand = async bindList => {
+    try {
+      const bindPromise = bindList.map(item => {
+        if (item.polling) {
+          this.pollingApiList.push(
+            setInterval(() => {
+              this.handleRequest(item);
+            }, item.polling),
+          );
+        }
+        return this.handleRequest(item);
+      });
+
+      return Promise.all(bindPromise);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  handleRequest = async ({ api, sendingData, formatBindingData, bindingData, callback }) => {
+    try {
+      const result = await api(sendingData);
+      // TODO 处理返回信息
+      /*if (result.status !== 6000) {
+        throw new Error(result.message);
+      }*/
+      // 申请接口不需要绑定值
+      if (bindingData) {
+        this.setState({
+          // [bindingData]: formatBindingData(result.data),
+          [bindingData]: formatBindingData(result),
+        });
+      }
+      callback(null, result);
+      return Promise.resolve(result);
+    } catch (e) {
+      callback(e);
+      return Promise.reject(e);
+    }
+  };
+
+  render() {
+    return null;
+  }
+}
+
+export default BasicServer;
---
+++ src/components/servers/loadingServer.js
@@ -0,0 +1,68 @@
+import BasicServer from './basicServer';
+
+const loadingType = {
+  global: 'global', // 全局
+  loading: 'loading', // 默认单个为loading
+};
+
+class LoadingServer extends BasicServer {
+  state = {};
+
+  bindType = {
+    all: 'all', // 一起请求
+  };
+
+  // 处理loading
+  handleRequestLoading = bindLoading => {
+    if (bindLoading === loadingType.global) {
+      // TODO 全局loading
+    } else if (bindLoading) {
+      this.setState({
+        [bindLoading]: true,
+      });
+    }
+  };
+  handleResponseLoading = bindLoading => {
+    if (bindLoading === loadingType.global) {
+      // TODO 全局loading
+    } else if (bindLoading) {
+      this.setState({
+        [bindLoading]: false,
+      });
+    }
+  };
+
+  bindAllAction = ({
+    api = {},
+    action = api.name,
+    type = this.bindType.all,
+    ...restProps
+  } = {}) => {
+    this.bindAction({
+      api,
+      action,
+      type,
+      ...restProps,
+    });
+  };
+  // 请求数据, 多个一起
+  getAll = ({ bindLoading = loadingType.global, type = this.bindType.all } = {}) => {
+    this.handleRequestLoading(bindLoading);
+    return this.doAction({ type }).finally(() => {
+      this.handleResponseLoading(bindLoading);
+    });
+  };
+  // 请求数据, 读取单个
+  getSingle = ({ bindLoading = loadingType.loading, action = '' } = {}) => {
+    this.handleRequestLoading(bindLoading);
+    return this.doAction({ action }).finally(() => {
+      this.handleResponseLoading(bindLoading);
+    });
+  };
+
+  render() {
+    return null;
+  }
+}
+
+export default LoadingServer;
